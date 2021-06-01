a923aa4c86c104a70ba982c86e11b443dc59b592
niceunderground <nikay360@gmail.com>
2021-06-01 17:54:32 +0200

    updated with news

--- src/Components/Pages/Receipt.js
+++ src/Components/Pages/Receipt.js
@@ -12,14 +12,23 @@ const Receipt = props => {
     return(
         <div>
             <h1>Receipt for {props.user.name}</h1>
-
             <input className="searchBar" type="text" value={search} onChange={searchHandler}/>
-            {/* Tabs => Primi - Secondi - Contorni */}
+            <ul>
+                <li>
+                    <p>Primo</p>
+                </li>
+                <li>
+                    <p>secondo</p>
+                </li>
+                <li>
+                    <p>Contorno</p>
+                </li>
+            </ul>
             {props.receipt.map( (meal, id) =>{
-
+                // some logic for => primo - secondo - contorno
                 return(
                     <span key={id} >
-                        <p>{meal.date}</p>
+                        <p>{meal.date.toString()}</p>
                         <p>{meal.name}</p>
                         <span>+</span>
                         <p>{meal.information}</p>
