65c552a1794042656cb04ec1e8bca87f91fa0816
plcdnl <polaccodaniele@gmail.com>
2021-06-01 11:22:37 +0200

    Navigation update

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,23 +1,23 @@
 import { NavLink } from 'react-router-dom';
-
+import account from '../Assets/imgs/account.svg';
 const Navigation = () =>{
     return(
         <nav>
             <ul className="menu">
                 <li className="menu__item">
-                    <NavLink activeClassName={'menu__item__link'} to='/calendar'>Calendat</NavLink>
+                    <NavLink activeClassName={'menu__item__link'} to='/calendar'><img src={account}></img></NavLink>
                 </li>
                 <li className="menu__item">
-                    <NavLink activeClassName={'menu__item__link'} to='/workout'>Workout</NavLink>
+                    <NavLink activeClassName={'menu__item__link'} to='/workout'><img src={account}></img></NavLink>
                 </li>
                 <li className="menu__item">
-                    <NavLink activeClassName={'menu__item__link'} to='/profile'>Profile</NavLink>
+                    <NavLink activeClassName={'menu__item__link'} to='/profile'><img src={account}></img></NavLink>
                 </li>
                 <li className="menu__item">
-                    <NavLink activeClassName={'menu__item__link'} to='/contactdoctor'>Contact doctor</NavLink>
+                    <NavLink activeClassName={'menu__item__link'} to='/contactdoctor'><img src={account}></img></NavLink>
                 </li>
                 <li className="menu__item">
-                    <NavLink activeClassName={'menu__item__link'} to='/receipt'>Receipt</NavLink>
+                    <NavLink activeClassName={'menu__item__link'} to='/receipt'><img src={account}></img></NavLink>
                 </li>
                 <li className="menu__item">
                     <span>logOut</span>
