766f2bd7d6742a01017c173d087f83acaa0712f7
niceunderground <nikay360@gmail.com>
2021-06-01 16:22:50 +0200

    beta of structure of receipts

--- src/Components/Pages/Receipt.js
+++ src/Components/Pages/Receipt.js
@@ -1,7 +1,31 @@
+import { useState } from "react"
+
 const Receipt = props => {
+
+    
+    const [search, setSearch] = useState("Search");
+
+    const searchHandler = event =>{
+        setSearch(event.target.value);
+    }
+
     return(
         <div>
             <h1>Receipt</h1>
+
+            <input className="searchBar" type="text" value={search} onChange={searchHandler}/>
+            {/* Tabs => Primi - Secondi - Contorni */}
+            {props.receipt.map( (meal, id) =>{
+
+                return(
+                    <span key={id} >
+                        <p>{meal.date}</p>
+                        <p>{meal.name}</p>
+                        <span><img /> span activate more informations</span>
+                        <p>receipt informations</p>
+                    </span>
+                )
+            } )}            
         </div>
         
     );
