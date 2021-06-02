bee63c808296767f5ace2947baf166e6a6f3a12a
plcdnl <polaccodaniele@gmail.com>
2021-06-02 09:20:32 +0200

    added card style

--- src/Components/Pages/Workout.js
+++ src/Components/Pages/Workout.js
@@ -2,9 +2,11 @@ import Card from '../card';
 
 const Workout = props => {
     return(
-        <div>
+        <div class="workout">
             <h1>Workout of {props.user.name}</h1>
+            <div class="workout__container">
             {props.lessions.map( (card, id) =>(<Card name={card.title} link={card.link} img={card.img} key={id} />))} 
+            </div>
         </div>
     );
 }
--- src/Components/card.js
+++ src/Components/card.js
@@ -1,8 +1,8 @@
 const Card = props =>{
     return(
         <a className="card" href={props.link} target="_blank" rel="noopener noreferrer">
-                <p className="card__title">{props.name}</p>
-                <img className="card___img" src={props.img} alt={props.name} />
+                <h3 className="card__title">{props.name}</h3>
+                <img className="card__img" src={props.img} alt={props.name} />
         </a>
     );
 }
