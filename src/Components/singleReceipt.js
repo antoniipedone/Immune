f86c8602d96a1fb9c334fe67c595ffd7475160b4
plcdnl <polaccodaniele@gmail.com>
2021-06-05 09:43:00 +0200

    some fix and other css style

--- src/Components/singleReceipt.js
+++ src/Components/singleReceipt.js
@@ -9,22 +9,28 @@ const SingleReceipt = props => {
             return(
                 <div className={`receipt ${active === id && "active"}`} onClick={() => setActive(id)} key={id} >
                     <div className="receipt__button">
-                        <p className="receipt__date">{`Time ${meal.prep + meal.coock} min`}</p>
+                        <p className="receipt__date">{`Time ${meal.prep + meal.cook} min`}</p>
                         <h4 className="receipt__name">{meal.name}</h4>
                     </div>
                     <div className="receipt__info" >
                         <img src={meal.img} alt={meal.name} />
-                        <p>{`Serves: ${meal.serves}`}</p>
-                        <p>{`Preparation: ${meal.prep} min`}</p>
-                        <p>{`Coock: ${meal.coock} min`}</p>
-                        <h4>Ingredients:</h4>
-                        <ul>
-                            {meal.ingredients.map( (ingredient, id) => (<li key={id}>{ingredient}</li>))}
-                        </ul>
-                        <h4>Process:</h4>
-                        <ol>
-                            {meal.process.map( (fase, id) => (<li key={id}>{fase}</li>))}
-                        </ol>
+                        <div className="receipt__info--heading">
+                            <p><span class="bold">Serves: </span> {meal.serves} </p>
+                            <p><span class="bold">Preparation: </span> {meal.prep} min.</p>
+                            <p><span class="bold">Cook: </span>{meal.cook} min.</p>
+                        </div>
+                        <div className="receipt__info--ingredients">
+                            <h4>Ingredients:</h4>
+                            <ul>
+                                {meal.ingredients.map( (ingredient, id) => (<li key={id}>{ingredient}</li>))}
+                            </ul>
+                        </div>
+                        <div className="receipt__info--process">
+                            <h4>Process:</h4>
+                            <ol>
+                                {meal.process.map( (fase, id) => (<li key={id}>{fase}</li>))}
+                            </ol>
+                        </div>
                     </div>
                     
                 </div>
